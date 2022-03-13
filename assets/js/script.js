cb5fdc45bba57281154ed9e06602e300ec9180aa
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-13 09:01:53 -0500

    work on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+$(document).ready(function() {
+
+
 var tasks = [];
 
 // time display/validation 
@@ -38,28 +41,35 @@ function createTasks(tasksText) {
 }
 
 // load all the tasks 
-var loadTasks = function() {
-    tasks = localStorage.getItem("tasks")
-}
+// var loadTasks = function() {
+//     tasks = localStorage.getItem("tasks")
+// }
 
 // save the tasks
-function saveTasks() {
-    $(".event-time").html(localStorage.mydata)
-};
+// function saveTasks() {
+//     $(".event-time").html(localStorage.text)
+// };
 
 // edit cards
-$(".event-time").on("click", function() {
-    var text = $(".description").val();
-    var dataHour = $("#data-hour").val();
-    localStorage.mydata =data
-});
+// $(".event-time").on("click", function() {
+//     var text = $(".description").val();
+//     var dataHour = $("#data-hour").val();
+//     localStorage.text = data
+// });
 
 // when save button is clicked 
-$(".saveBtn").click(function() {
-    var text = $(".description").val();
-    localStorage.setItem($(".description").parent().attr("data-hour"), text);
+$(".saveBtn").on("click",function() {
+    var text = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(hour, text);
     console.log(text);
-    saveTasks();
+    // saveTasks();
 });
 
-loadTasks(); 
+// loadTasks(); 
+
+
+
+$("#9 .description").val(localStorage.getItem("9"));
+
+})
